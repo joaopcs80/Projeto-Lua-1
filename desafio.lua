e1c5630b4e33b0d5022e6a39c8fa6328719cd570
Dev002 - Joao Paulo <ddev02@domtec.svr>
2024-05-29 10:31:39 -0300

    'main'

---
+++ desafio.lua
@@ -0,0 +1,42 @@
+--[[
+
+  Nome da Criatura
+  Descrição
+  Atributos
+    Ataque
+    Defesa
+    Vitalidade
+    Velocidade
+    Inteligência
+  Habilidades
+    Furtividade
+    Explosão
+
+==========================================================
+/
+/  CREEPER
+/  Um monstro muito sagaz que epxlode na sua cara.
+/
+/  Som: Tssss
+/
+/  Atributos:
+/    Ataque: ########00
+/    Defesa: ###0000000
+/
+/
+===========================================================
+
+]]
+
+print("==========================================================")
+print("/")
+print("/  CREEPER")
+print("/  Um monstro muito sagaz que epxlode na sua cara.")
+print("/")
+print("/  Som: Tssss")
+print("/")
+print("/  Atributos:")
+print("/    Ataque: ########00")
+print("/    Defesa: ###0000000")
+print("/")
+print("===========================================================")
